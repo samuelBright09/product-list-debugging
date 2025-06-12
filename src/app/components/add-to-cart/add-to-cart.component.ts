d20d58a0f36c4d0e9b891e0151b1b0d367128ed4
samuelBright09 <samuelbrightsk@gmail.com>
2025-06-12 12:17:04 +0000

    Add functionality and refactored add to cart functionality

--- src/app/components/add-to-cart/add-to-cart.component.ts
+++ src/app/components/add-to-cart/add-to-cart.component.ts
@@ -1,30 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, OnDestroy } from '@angular/core';
+import { Dessert } from '../../../shared/interfaces';
+import { CartService } from '../../services/cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-to-cart',
   templateUrl: './add-to-cart.component.html',
   styleUrl: './add-to-cart.component.scss'
 })
-
-
-
-export class AddToCartComponent {
+export class AddToCartComponent implements OnInit, OnDestroy {
+  @Input() product!: Dessert;
+  @Output() quantityChange = new EventEmitter<{product: Dessert, quantity: number}>();
+  
   isAddedToCart = false;
   quantity = 1;
+  private removedItemSubscription: Subscription;
+
+  constructor(private cartService: CartService) {
+    this.removedItemSubscription = this.cartService.removedItem$.subscribe(removedItemName => {
+      if (removedItemName === this.product.name) {
+        this.isAddedToCart = false;
+        this.quantity = 1;
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    // Subscribe to cart items to initialize isAddedToCart state
+    this.cartService.getCartItems().subscribe(items => {
+      const item = items.find(item => item.product.name === this.product.name);
+      this.isAddedToCart = !!item;
+      if (item) {
+        this.quantity = item.quantity;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.removedItemSubscription.unsubscribe();
+  }
 
   addToCart() {
     this.isAddedToCart = true;
+    this.quantityChange.emit({product: this.product, quantity: this.quantity});
   }
 
   decreaseProductItem() {
-    if (this.quantity < 1) {
+    if (this.quantity > 1) {
+      this.quantity--;
+      this.quantityChange.emit({product: this.product, quantity: this.quantity});
+    } else {
       this.isAddedToCart = false;
+      this.quantity = 1;
+      this.quantityChange.emit({product: this.product, quantity: 0});
     }
-    this.quantity--;
   }
 
   increaseProductItem() {
-    ++this.quantity;
+    this.quantity++;
+    this.quantityChange.emit({product: this.product, quantity: this.quantity});
   }
-
-};
+}
